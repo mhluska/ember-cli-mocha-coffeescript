8bae56b5bebf97e6e06b4178a8a4c480f22b5628
Ben Limmer <ben@benlimmer.com>
2015-03-26 15:03:44 -0600

    Check coffeeOptions in config before including blueprint.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-cli-mocha-coffeescript'
+  name: 'ember-cli-mocha-coffeescript',
+
+  blueprintsEnabled: function() {
+    var config = this.project.config(process.env.EMBER_ENV).coffeeOptions.blueprints;
+    if (config !== undefined) {
+      return config;
+    } else {
+      return true;
+    }
+  },
+
+  blueprintsPath: function() {
+    if (this.blueprintsEnabled()) {
+      return path.join(__dirname, 'blueprints');
+    }
+  }
 };
